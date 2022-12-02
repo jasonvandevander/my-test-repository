6e0ff47e60f9112076b900ec8262462b10debd0f
Jason Vandevander <jasonvan84@gmail.com>
2022-12-02 15:46:33 -0500

    "new buttons"

--- project02/app.js
+++ project02/app.js
@@ -11,3 +11,21 @@ function newItem() {
 }
 
 
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "remove";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var remove = document.getElementsByClassName("remove");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
